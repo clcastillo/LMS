51f2af40f34b4eed17988940ab8be98618a75cb7
Robbie <39038445+keyblader89@users.noreply.github.com>
2018-12-06 16:58:36 -0800

    added searchID() and changed method return types from Item to void

--- User.java
+++ User.java
@@ -6,69 +6,83 @@ public class User extends Library implements Serializable
 	private static final long serialVersionUID = -16820323213541553L;
 	Scanner keyboard = new Scanner(System.in);
 	int userChoice;
+	String input;
 	boolean status;
 
 	public int prompt() {
 		do {
 			System.out.println();
 			System.out.println("Following Functionalities are available: \n");
-			System.out.println("1- Search an Item");
-			System.out.println("2- Search for an Author");
-			System.out.println("3- View all items");
-			System.out.println("4- Check out an item");
-			System.out.println("5- Return a book");
-			System.out.println("6- Logout");
+			System.out.println("1- Search library by title");
+			System.out.println("2- Search library by library ID");
+			System.out.println("3- Search books by Author");
+			System.out.println("4- View all items");
+			System.out.println("5- Check out an item");
+			System.out.println("6- Return a book");
+			System.out.println("7- Logout");
+			
 			System.out.println("---------------------------------------------");
-			userChoice = takeInput(0, 7);
+			userChoice = takeInput(0, 8);
 			return userChoice;
-		} while (userChoice != 6);
+		} while (userChoice != 7);
 	}
-	public Item searchTitle() {
+	public void searchTitle() 
+	{
 		System.out.print("Enter item title to search:");
-		String title = keyboard.nextLine().toUpperCase();
-		for (int i = 0; i < catalogList.size(); i++) {
-			// System.out.println(((Book) catalogList.get(i)).getTitle() + "= " + title);
-			if (((Book) catalogList.get(i)).getTitle().equals(title)) {
+		input = keyboard.nextLine().toUpperCase();
+		for (int i = 0; i < catalogList.size(); i++) 
+		{
+			if (((Item) catalogList.get(i)).getTitle().equals(input)) 
+			{
 				System.out.println("Its in the library: Item " + (i + 1));
 				status = true;
 			}
 		}
-		if (status = false) {
+		
+		if (status = false) 
 			System.out.println("Sorry, we don't have that item");
-		}
-		return null;
-
 	}
 
-	public Item searchAuthor() {
-		System.out.println("Enter item author to search:");
-		String author = keyboard.nextLine().toUpperCase();
-		for (int i = 0; i < catalogList.size(); i++) {
-			// System.out.println(((Book) catalogList.get(i)).getTitle() + "= " + title);
-			if (((Book) catalogList.get(i)).getAuthor().equals(author)) {
+	public void searchAuthor() {
+		System.out.println("Enter author to search:");
+		input = keyboard.nextLine().toUpperCase();
+		for (int i = 0; i < catalogList.size(); i++) 
+		{
+			if (((Book) catalogList.get(i)).getAuthor().equals(input)) 
 				System.out.println("Its in the library: Item " + (i + 1));
+		}
+	}
+	
+	public void searchID() {
+		System.out.print("Enter item ID to search:");
+		input = keyboard.nextLine().toUpperCase();
+		for (int i = 0; i < catalogList.size(); i++) 
+		{
+			if (((Item) catalogList.get(i)).getID().equals(input)) 
+			{
+				System.out.println("Its in the library: Item " + (i + 1));
+				status = true;
 			}
 		}
-		return null;
+		if (status = false)
+			System.out.println("Sorry, we don't have that item");
 	}
 
-	public Item checkOut() {
+	public void checkOut() 
+	{
 		System.out.println("Enter the item number to check out:");
-		//int checkOut = keyboard.nextInt();
 		userChoice= keyboard.nextInt();
 		((Book) catalogList.get(userChoice - 1)).setCheckedOut();
 		System.out.println("Done.");
-		return null;
 	}
 
-	public Item returnItem() {
+	public void returnItem() 
+	{
 		System.out.print("Enter the item number to return:");
-		//int returnItem = keyboard.nextInt();
+		
 		userChoice= keyboard.nextInt();
 		((Book)catalogList.get(userChoice- 1)).setReturned();
 		System.out.println("Done.");
-
-		return null;
 	}
 
 	public void getItems() {
