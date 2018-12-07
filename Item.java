de07425e08c29c58f8bed5b5330f30c37d6eb2e8
Robbie <39038445+keyblader89@users.noreply.github.com>
2018-12-06 22:27:45 -0800

    added a method to prevent duplicate IDs

--- Item.java
+++ Item.java
@@ -41,20 +41,40 @@ public abstract class Item extends LibraryCat implements Serializable
 			return false;
 
 	}
+	
+	public boolean dupeID(String id)
+	{
+		boolean dupe= false;
+		for (int i = 0; i < catalogList.size(); i++) 
+		{
+			if (((Item) catalogList.get(i)).getID().equals(id)) 
+			{
+				dupe= true;
+				break;
+			}
+		}
+		
+		return dupe;
+	}
 	public void addItem(LibraryCat lib)
 	{
 		catalogList.add(lib);
 	}
 	protected void requestInfo() 
 	{
+		String tempID; 
 		System.out.print("Title: ");
 		title = keyboard.nextLine().toUpperCase();
 		do
 		{
 			System.out.print("Library ID: ");
-			libID= keyboard.nextLine();
-			if(libID.length()>6 || libID.length() == 0)
+			tempID= keyboard.nextLine();
+			if(tempID.length()>6 || tempID.length() == 0)
 				System.out.println("Please enter a 6 character alphanumeric ID");
-		}while(libID.length()>6 || libID.length() == 0);
+			if(dupeID(tempID))
+				System.out.println("ID " + tempID + " already exists in library. Please enter another ID. ");
+		}while(tempID.length()>6 || tempID.length() == 0 || dupeID(tempID));
+		
+		libID= tempID;
 	}
 }
